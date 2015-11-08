70281144e7368409fa90ed9c12cac8e0bd85cf3b
alexbondor <alexandru.viorel.bondor@gmail.com>
2015-11-08 20:46:46 +0200

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>alexbondor</title>
+
+        <link href='https://fonts.googleapis.com/css?family=Inconsolata' rel='stylesheet' type='text/css'>
+
+        <style>
+            html, body {
+                height: 100%;
+            }
+
+            body {
+                margin: 10px;
+                padding: 0px;
+                width: 100%;
+                font-weight: 100;
+                font-family: 'Inconsolata';
+            }
+
+            .title {
+                font-size: 40px;
+            }
+
+            .subtitle {
+                font-size: 30px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="title"><u>Alex Bondor / Hacker</u></div>
+        <div class="subtitle">Cluj-Napoca, România</div>
+    </body>
+</html>
