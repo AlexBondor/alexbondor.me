ef362f2b53e9794520fae9f584032ae7e1583078
alexbondor <alexandru.viorel.bondor@gmail.com>
2015-11-09 23:51:57 +0200

    Nice name & title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,31 +5,9 @@
 
         <link href='https://fonts.googleapis.com/css?family=Inconsolata' rel='stylesheet' type='text/css'>
         <link href="{{ asset('/css/custom.css') }}" rel="stylesheet">
-
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 10px;
-                padding: 0px;
-                width: 100%;
-                font-weight: 100;
-                font-family: 'Inconsolata';
-            }
-
-            .title {
-                font-size: 40px;
-            }
-
-            .subtitle {
-                font-size: 30px;
-            }
-        </style>
     </head>
-    <body class="welcome-body">
-        <div class="title"><u>Alex Bondor / Computer Scientist</u></div>
-        <div class="subtitle">Cluj-Napoca, România</div>
+    <body>
+        <div class="name"><u>Alex Bondor</u></div>
+        <div class="title">Computer Scientist</div>
     </body>
 </html>
