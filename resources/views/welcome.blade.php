f9a71fbc798d3d469d0c7a5fc6724c6bef335566
alexbondor <alexandru.viorel.bondor@gmail.com>
2015-11-16 20:36:45 +0200

    Google analytics

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,11 +4,37 @@
         <title>alexbondor</title>
 
         <link href='https://fonts.googleapis.com/css?family=Inconsolata' rel='stylesheet' type='text/css'>
+        <link href="{{ asset('/css/bootstrap.min.css') }}" rel="stylesheet">
         <link href="{{ asset('/css/custom.css') }}" rel="stylesheet">
     </head>
     <body>
-        <div class="name"><u>Alex Bondor</u></div>
-        <div class="title">Computer Scientist</div>
-        <div class="subtitle">Doer|Creator|Persuader</div>	
+        <div class="row">
+            <div class="col-xs-4">
+                <img class="self-portrait" src="{{ asset('/images/me.png') }}"/>
+                <div class="name center-text">Alex Bondor</div>
+                <div class="title center-text">Computer Scientist</div>
+                <div class="subtitle center-text">Doer|Creator|Persuader</div>
+            </div>
+            <div class="col-xs-8">
+                <div class="title">Current project</div>
+                <div class="text">
+                    Hide head under blanket so no one can see chew on cable, make muffins scratch leg; meow for can opener to feed me curl into a furry donut. Leave fur on owners clothes love to play with owner's hair tie. Get video posted to internet for chasing red dot human is washing you why halp oh the horror flee scratch hiss bite and eat grass, throw it back up tuxedo cats always looking dapper or jump off balcony, onto stranger's head. White cat sleeps on a black shirt. Destroy the blinds chase the pig around the house but where is my slave? I'm getting hungry for kitty power! , steal the warm chair right after you get up. Refuse to leave cardboard box stick butt in face, hide at bottom of staircase to trip human. Where is my slave? I'm getting hungry sit by the fire for bathe private parts with tongue then lick owner's face.
+                </div>
+                <div class="title">Last project</div>
+                <div class="text">
+                    Hide head under blanket so no one can see chew on cable, make muffins scratch leg; meow for can opener to feed me curl into a furry donut. Leave fur on owners clothes love to play with owner's hair tie. Get video posted to internet for chasing red dot human is washing you why halp oh the horror flee scratch hiss bite and eat grass, throw it back up tuxedo cats always looking dapper or jump off balcony, onto stranger's head. White cat sleeps on a black shirt. Destroy the blinds chase the pig around the house but where is my slave? I'm getting hungry for kitty power! , steal the warm chair right after you get up. Refuse to leave cardboard box stick butt in face, hide at bottom of staircase to trip human. Where is my slave? I'm getting hungry sit by the fire for bathe private parts with tongue then lick owner's face.
+                </div>
+            </div>
+        </div>
     </body>
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-63171754-2', 'auto');
+        ga('send', 'pageview');
+
+    </script>
 </html>
